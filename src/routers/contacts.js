93e3bcd819d9728f3297cc8b1e82202299a28f61
Denis <denistaran482@gmail.com>
2025-01-29 01:13:52 +0200

    Add Joi validation and ID validation

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -8,14 +8,29 @@ import {
   getContactByIdController,
   updateContactController,
 } from '../controllers/contacts.js';
+import { validateBody } from '../middlewares/validateBody.js';
+import {
+  createContactValidationShcema,
+  updateContactValidationShcema,
+} from '../validation/contacts.js';
+import { isValidId } from '../middlewares/isValidId.js';
 
 const router = Router();
 
 router.get('/', ctrlWrapper(homeController));
 router.get('/contacts', ctrlWrapper(getAllContactsController));
-router.get('/contacts/:contactId', ctrlWrapper(getContactByIdController));
-router.post('/contacts', ctrlWrapper(createContactController));
-router.patch('/contacts/:contactId', ctrlWrapper(updateContactController));
-router.delete('/contacts/:contactId', ctrlWrapper(deleteContactController));
+router.get('/contacts/:contactId', isValidId, ctrlWrapper(getContactByIdController));
+router.post(
+  '/contacts',
+  validateBody(createContactValidationShcema),
+  ctrlWrapper(createContactController),
+);
+router.patch(
+  '/contacts/:contactId',
+  isValidId,
+  validateBody(updateContactValidationShcema),
+  ctrlWrapper(updateContactController),
+);
+router.delete('/contacts/:contactId', isValidId, ctrlWrapper(deleteContactController));
 
 export default router;
---
+++ src/validation/contacts.js
@@ -0,0 +1,44 @@
+import Joi from 'joi';
+
+export const createContactValidationShcema = Joi.object({
+  name: Joi.string().min(3).max(20).required().messages({
+    'string.base': 'Usernmae should be a string',
+    'string.min': 'Username should have at least {#limit} characters',
+    'string.max': 'Username should have at most {#limit} characters',
+    'any.required': 'Username is required',
+  }),
+  phoneNumber: Joi.string()
+    .min(3)
+    .max(20)
+    .pattern(/^\+380\d{9}$/)
+    .required()
+    .messages({
+      'string.base': 'Phone number should be a string',
+      'string.pattern.base': 'Phone number must match the format +380XXXXXXXXX',
+      'any.required': 'Phone number is required',
+    }),
+  email: Joi.string().min(3).max(20).email().required(),
+  isFavourite: Joi.boolean(),
+  contactType: Joi.string().valid('work', 'home', 'personal').required(),
+});
+
+export const updateContactValidationShcema = Joi.object({
+  name: Joi.string().min(3).max(20).messages({
+    'string.base': 'Usernmae should be a string',
+    'string.min': 'Username should have at least {#limit} characters',
+    'string.max': 'Username should have at most {#limit} characters',
+    'any.required': 'Username is required',
+  }),
+  phoneNumber: Joi.string()
+    .min(3)
+    .max(20)
+    .pattern(/^\+380\d{9}$/)
+    .messages({
+      'string.base': 'Phone number should be a string',
+      'string.pattern.base': 'Phone number must match the format +380XXXXXXXXX',
+      'any.required': 'Phone number is required',
+    }),
+  email: Joi.string().min(3).max(20).email(),
+  isFavourite: Joi.boolean(),
+  contactType: Joi.string().valid('work', 'home', 'personal'),
+});
